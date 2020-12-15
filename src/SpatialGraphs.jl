8e0ebbd53cb9eddd608f1526565d3c0f4f88bd4e
Vincent Landau <vincent@csp-inc.org>
2020-12-14 20:32:27 -0700

    export a couple more functions

--- src/SpatialGraphs.jl
+++ src/SpatialGraphs.jl
@@ -5,6 +5,6 @@ include("graphs.jl")
 export construct_nodemap, construct_graph
 
 include("paths.jl")
-export sample_lcp_node_pairs, 
+export sample_lcp_node_pairs, compute_cd_graph, least_cost_path
 
 end # module
